1475879b71a8be636d57054ddf1208c10f72e149
sabrina-wen <sabrinawn253@gmail.com>
2017-11-02 19:19:22 -0400

    in progress

--- directories.c
+++ directories.c
@@ -1,2 +1,14 @@
 #include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
 
+int main() {
+  DIR * d;
+  d = opendir("/");
+  printf("Statistics of directory: .\n");
+  struct dirent * entry;
+  entry = readdir(d);
+  closedir(d);
+
+  return 0;
+}
